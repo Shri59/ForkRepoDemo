52fbee460b2be3f7287c17ff8b1eb55e07e2909a
Supriya Singh <supriya.i.singh@capgemini.com>
2025-02-12 12:43:07 +0530

    new property added

--- SupriyaDemo.cs
+++ SupriyaDemo.cs
@@ -9,5 +9,6 @@ namespace EMSDemo
     public class SupriyaDemo
     {
         public string Address { get; set; } //Address added by Supriya
+        public int FriendIDNumber { get; set; } //new property added by Supriya
     }
 }
