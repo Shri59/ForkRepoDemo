2804b5e36fbb482abb5ecf5434aabcb4c45af01f
Tejashri Shinde <tejashri.shivajishinde@capgemini.com>
2025-02-12 12:27:38 +0530

    Phone No Properrty Added

--- TejashriDemo.cs
+++ TejashriDemo.cs
@@ -10,7 +10,7 @@ namespace EMSDemo
     {
         public string Name { get; set; }//Name Property created
 
-        public int PhoneNo { get; set; } //Phone number
+        public int PhoneNo { get; set; } //Phone number Added
 
     }
 }
